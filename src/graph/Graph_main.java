72d9e380c448dc5c0c4c1b455d32f1916b773c42
50616030 <50616030@50616030-PC>
2018-01-09 12:51:31 +0900

    graph 50616030

---
+++ src/graph/Graph_main.java
@@ -0,0 +1,12 @@
+package graph;
+
+public class Graph_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Graph_view frame = new Graph_view(0);
+        frame.setBounds(5,5,655,455);
+        frame.setVisible(true);
+	}
+
+}
